25ac9461cdea23be04b3746a7fd9b6d20aa91b96
Geoff <geoffbeamish@gmail.com>
2021-02-01 19:17:12 -0500

    refactor for OO design

---
+++ webapp/emoji_classifier.py
@@ -0,0 +1,26 @@
+from fastai.learner import load_learner
+from webapp.model_fetcher import download_file_from_google_drive
+import os
+from pathlib import Path
+
+
+class EmojiClassifier:
+    def __init__(self):
+        self.MODEL_PATH = "model.pkl"
+        if not Path(self.MODEL_PATH).is_file():
+            self.MODEL_STORAGE_ID = os.environ.get("MODEL_STORAGE_ID")
+            if self.MODEL_STORAGE_ID:
+                print(f"Fetching model of ID: {self.MODEL_STORAGE_ID}")
+                download_file_from_google_drive(self.MODEL_STORAGE_ID, self.MODEL_PATH)
+            else:
+                raise KeyError("Error - MODEL_STORAGE_ID not set. Check Heroku config")
+
+        print(f"Loading model {self.MODEL_PATH}")
+        self.learn = load_learner(self.MODEL_PATH)
+        self.labels = self.learn.dls.vocab[1]
+
+    def classify_emoji(self, text: str):
+        confidences = self.learn.predict(text)[2].tolist()
+        # Wrap labels and confidences into dictionary
+        results = {l: c for l, c in zip(self.labels, confidences)}
+        return sorted(results.items(), key=lambda x: x[1], reverse=True)
--- webapp/serve.py
+++ webapp/serve.py
@@ -1,17 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-from fastai.learner import load_learner
-
-from prettytable import PrettyTable
 from typing import Optional
-from pathlib import Path
-import os
+from prettytable import PrettyTable
 
-from webapp.model_fetcher import download_file_from_google_drive
+from webapp.emoji_classifier import EmojiClassifier
 
 # Bind webserver early to avoid timeout issue
 app = FastAPI()
+model = EmojiClassifier()
 
 
 @app.get("/")
@@ -19,20 +16,9 @@ async def root():
     return {"message": "Hello World"}
 
 
-# Warm up
-MODEL_PATH = "model.pkl"
-if not Path(MODEL_PATH).is_file():
-    MODEL_STORAGE_ID = os.environ.get("MODEL_STORAGE_ID")
-    if MODEL_STORAGE_ID:
-        print(f"Fetching model of ID: {MODEL_STORAGE_ID}")
-        download_file_from_google_drive(MODEL_STORAGE_ID, MODEL_PATH)
-    else:
-        raise KeyError("Error - MODEL_STORAGE_ID not set. Check Heroku config")
-
-# Load Learner from model
-print(f"Loading model {MODEL_PATH}")
-learn = load_learner(MODEL_PATH)
-labels = learn.dls.vocab[1]
+@app.get("/healthcheck", status_code=200)
+async def healthcheck():
+    return "Emoji classifier is all ready to go!"
 
 
 class Text(BaseModel):
@@ -44,10 +30,7 @@ def classify_text(text: Text, max_n=5):
     output_grid = PrettyTable(format=True)
     output_grid.field_names = ["Emoji", "Confidence"]
     # Get List of predictions from fastai
-    confidences = learn.predict(text.body)[2].tolist()
-    # Wrap labels and confidences into dictionary
-    results = {l: c for l, c in zip(labels, confidences)}
-    results = sorted(results.items(), key=lambda x: x[1], reverse=True)
+    results = model.classify_emoji(text.body)
     top_results = results[:max_n]
     # TODO: Use this to display confidence in pred
     total_confidence = sum([x[1] for x in top_results])

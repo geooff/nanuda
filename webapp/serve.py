54fe5113bfa95a9c27f50426a087fc8d77ffd745
Geoff <geoffbeamish@gmail.com>
2021-02-04 22:21:09 -0500

    remove table maker

--- webapp/serve.py
+++ webapp/serve.py
@@ -8,7 +8,13 @@ from webapp.emoji_classifier import EmojiClassifier
 
 # Bind webserver early to avoid timeout issue
 app = FastAPI()
+
+# Init our classifier
 model = EmojiClassifier()
+
+
+class Text(BaseModel):
+    body: str
 
 
 @app.get("/")
@@ -16,24 +22,16 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/healthcheck", status_code=200)
-async def healthcheck():
-    return "Emoji classifier is all ready to go!"
-
-
-class Text(BaseModel):
-    body: str
-
-
-@app.post("/predict")
-def classify_text(text: Text, max_n=5):
-    output_grid = PrettyTable(format=True)
-    output_grid.field_names = ["Emoji", "Confidence"]
+@app.post("/")
+async def classify_text(text: Text, max_n=5):
     # Get List of predictions from fastai
     results = model.classify_emoji(text.body)
     top_results = results[: int(max_n)]
     # TODO: Use this to display confidence in pred
     total_confidence = sum([x[1] for x in top_results])
-    for row in top_results:
-        output_grid.add_row(row)
-    return output_grid.get_html_string()
+    return top_results
+
+
+@app.get("/healthcheck", status_code=200)
+async def healthcheck():
+    return "Emoji classifier is all ready to go!"

d600389f5c988d95e4a8b60fed4f7ba475889901
Amun Sihra <>
2021-02-25 20:20:38 -0500

    Random background colour generator

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -23,7 +23,16 @@ class Home extends Component {
       body: "",
       results: [],
       chars_left: this.max_chars,
+      backgroundColours: [],
     };
+  }
+  componentDidMount() {
+    const colours = [];
+    const baseHex = this.generateRandomHexColour();
+    for (var i = 0; i < 5; i++) {
+      colours.push(this.generateShades(baseHex));
+    }
+    this.setState({ backgroundColours: colours });
   }
 
   instance = axios.create({
@@ -59,20 +68,40 @@ class Home extends Component {
       });
   };
 
+  generateShades = (color) => {
+    var p = 1,
+      temp,
+      random = Math.random(),
+      result = "#";
+
+    while (p < color.length) {
+      temp = parseInt(color.slice(p, (p += 2)), 16);
+      temp += Math.floor((255 - temp) * random);
+      result += temp.toString(16).padStart(2, "0");
+    }
+    return result;
+  };
+
+  generateRandomHexColour = () => {
+    const randomColour = Math.floor(Math.random() * 16777215).toString(16);
+    const hexValue = "#" + randomColour;
+
+    return this.generateShades(hexValue);
+  };
+
   render() {
     require("./styles.css");
-    const { results } = this.state;
+    const { results, backgroundColours } = this.state;
+    const divStyle = {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      background: `linear-gradient(-45deg,${backgroundColours[0]},${backgroundColours[1]},
+        ${backgroundColours[2]}, ${backgroundColours[3]}, ${backgroundColours[4]})`,
+    };
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          background:
-            "linear-gradient(-45deg, #a1adc9, #637fc2, #1e46a6, #6e7085)",
-        }}
-      >
+      <div style={divStyle}>
         <form onSubmit={this.handleSubmit}>
           <Grid container spacing={3}>
             <Grid item xs={12}>
@@ -87,7 +116,7 @@ class Home extends Component {
                 className="charsRemaining"
                 style={
                   this.state.chars_left >= 15
-                    ? { color: "white", marginTop: -20 }
+                    ? { color: "black", marginTop: -20 }
                     : { color: "red", marginTop: -20 }
                 }
               >

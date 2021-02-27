93c299b229f60714feefb8c32292f5eb8c2ac303
Geoff <geoffbeamish@gmail.com>
2021-02-27 10:49:25 -0500

    Push missed changes

--- backend/utils/crud.py
+++ backend/utils/crud.py
@@ -11,14 +11,9 @@ async def create_prediction(
     db: databases, table: Table, classify: schemas.ClassifyCreate
 ):
     classify = classify.dict()
-    json_raw_result = json.dumps(classify.pop("raw_result"))
     json_result = json.dumps(classify.pop("result"))
     query = table.insert().values(
-        id=uuid4(),
-        created_at=datetime.now(),
-        raw_result=json_raw_result,
-        result=json_result,
-        **classify
+        id=uuid4(), created_at=datetime.now(), result=json_result, **classify
     )
     last_record_id = await db.execute(query)
     return last_record_id

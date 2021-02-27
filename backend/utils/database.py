05d220e70b91796eeee4b1b1123c8cb6a53e2ee0
Geoff <geoffbeamish@gmail.com>
2021-02-26 19:46:16 -0500

    Try to fix db_conn pooling

--- backend/utils/database.py
+++ backend/utils/database.py
@@ -6,9 +6,11 @@ from sqlalchemy import create_engine, MetaData
 
 metadata = MetaData()
 
+db_args = {"ssl": True, "min_size": 5, "max_size": 20}
+
 if os.getenv("DB_CONN_STR"):
     DATABASE_URL = os.getenv("DB_CONN_STR")
-    db = databases.Database(DATABASE_URL)
+    db = databases.Database(DATABASE_URL, **db_args)
     engine = create_engine(DATABASE_URL)
 else:
     DATABASE_URL = "sqlite:///./sql_app.db"
